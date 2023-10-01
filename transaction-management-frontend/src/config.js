e304b56b6cd19369dd5a26bc1e03a7cb8470d081
sumj25 <sumanj252001@gmail.com>
2023-10-01 19:32:43 +0530

    inital

--- transaction-management-frontend/src/config.js
+++ transaction-management-frontend/src/config.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://flagright-transactions-backend2.onrender.com";
-export const baseUrl = "http://localhost:3001";
+export const baseUrl = "https://flagright.onrender.com";
+
